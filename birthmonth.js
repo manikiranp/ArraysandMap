845c8253d6857cea85e7a351ab5e1bd12ce5b7a9
manikiranp <manikiranp3@gmail.com>
2020-12-05 16:07:19 +0530

    Added:storing same birth month into a map and printing it

---
+++ birthmonth.js
@@ -0,0 +1,73 @@
+var i;
+var MONTH;
+var map=new Map();
+var c1=0, c2=0, c3=0,c4=0,c5=0,c6=0;c7=0;c8=0;c9=0,c10=0,c11=0,c12=0;
+var n=0;
+while (n<50) {
+ MONTH=Math.floor(Math.random()*12)+1;
+ //console.log("Dice roll: "+ROLL)
+	switch (MONTH) {
+		case 1:
+			c1++;
+			map.set('Jan',c1);
+			break;
+		case 2:
+			c2++;
+			map.set('Feb',c2);
+			break;
+		case 3:
+			c3++;
+			map.set('Mar',c3)
+			break;
+		case 4:
+			c4++;
+			map.set('Apr',c4)
+			break;
+		case 5:
+			c5++;
+			map.set('May',c5)
+			break;
+		case 6:
+			c6++;
+			map.set('Jun',c6)
+			break;
+		case 7:
+			c7++;
+			map.set('Jul',c7)
+			break;
+		case 8:
+			c8++;
+			map.set('Aug',c8)
+			break;
+		case 9:
+			c9++;
+			map.set('Sep',c9)
+			break;
+		case 10:
+			c10++;
+			map.set('Oct',c10)
+			break;
+		case 11:
+			c11++;
+			map.set('Nov',c11)
+			break;
+		case 12:
+			c12++;
+			map.set('Dec',c12)
+			break;
+	//	default:
+	//		console.log("Run again")
+	//		break;
+		}
+		n++;
+	}
+	console.log("Number of people having birth in same month");
+	for (let [key,count] of map.entries()) {
+		console.log(`Month ${key}: ${count}`);
+
+	}
+	
+	
+ 	
+
+
--- dice_roll.js
+++ dice_roll.js
@@ -6,6 +6,7 @@ var c1=0, c2=0, c3=0,c4=0,c5=0,c6=0;
 while ( (c1<10) && (c2<10) && (c3<10) && (c4<10) && (c5<10) && (c6<10) ) {
  ROLL 	=Math.floor(Math.random()*6)+1;
 	//console.log("Dice roll: "+ROLL)
+
 	switch (ROLL) {
 		case 1:
 			c1++;
@@ -42,7 +43,3 @@ while ( (c1<10) && (c2<10) && (c3<10) && (c4<10) && (c5<10) && (c6<10) ) {
 
 	}
 	
-	
- 	
-
-
